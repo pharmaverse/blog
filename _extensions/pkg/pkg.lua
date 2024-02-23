e6474ffda36ca1cf0dad59e78ed082602c92abf7
Romain Francois <romain@purrple.cat>
2024-02-23 12:29:08 +0100

    + quarto shortcode extension pkg

---
+++ _extensions/pkg/pkg.lua
@@ -0,0 +1,92 @@
+# generated with pharmaversedown::write_pkg_info()
+pkg_info = {}
+pkg_info["teal.reporter"] = { url = "https://insightsengineering.github.io/teal.reporter/", title = "Reporting Tools for 'shiny' Modules" }
+pkg_info["rtables"] = { url = "https://insightsengineering.github.io/rtables/", title = "Reporting Tables" }
+pkg_info["teal"] = { url = "https://insightsengineering.github.io/teal/", title = "Exploratory Web Apps for Analyzing Clinical Trials Data" }
+pkg_info["xportr"] = { url = "https://atorus-research.github.io/xportr/", title = "Utilities to Output CDISC SDTM/ADaM XPT Files" }
+pkg_info["tern"] = { url = "https://insightsengineering.github.io/tern/", title = "Create Common TLGs Used in Clinical Trials" }
+pkg_info["teal.modules.clinical"] = { url = "https://insightsengineering.github.io/teal.modules.clinical/", title = "'teal' Modules for Standard Clinical Outputs" }
+pkg_info["r.pkg.template"] = { url = "https://github.com/insightsengineering/r.pkg.template/", title = "R Package Template" }
+pkg_info["tfrmt"] = { url = "https://GSK-Biostatistics.github.io/tfrmt/", title = "Applies Display Metadata to Analysis Results Datasets" }
+pkg_info["simaerep"] = { url = "https://openpharma.github.io/simaerep/", title = "Find Clinical Trial Sites Under-Reporting Adverse Events" }
+pkg_info["teal.modules.hermes"] = { url = "https://insightsengineering.github.io/teal.modules.hermes/", title = "RNA-Seq Analysis Modules to Add to a Teal Application" }
+pkg_info["teal.modules.general"] = { url = "https://insightsengineering.github.io/teal.modules.general/", title = "General Modules to Add to a `teal` Application" }
+pkg_info["Tplyr"] = { url = "https://github.com/atorus-research/Tplyr", title = "A Traceability Focused Grammar of Clinical Data Summary" }
+pkg_info["formatters"] = { url = "https://insightsengineering.github.io/formatters/", title = "ASCII Formatting for Values and Tables" }
+pkg_info["teal.goshawk"] = { url = "https://insightsengineering.github.io/teal.goshawk/", title = "Longitudinal Visualization `teal` Modules" }
+pkg_info["mmrm"] = { url = "https://openpharma.github.io/mmrm/", title = "Mixed Models for Repeated Measures" }
+pkg_info["haldensify"] = { url = "https://github.com/nhejazi/haldensify", title = "Highly Adaptive Lasso Conditional Density Estimation" }
+pkg_info["teal.slice"] = { url = "https://insightsengineering.github.io/teal.slice/", title = "Filter Module for 'teal' Applications" }
+pkg_info["teal.transform"] = { url = "https://insightsengineering.github.io/teal.transform/", title = "Functions for Extracting and Merging Data in the 'teal' Framework" }
+pkg_info["tern.mmrm"] = { url = "https://insightsengineering.github.io/tern.mmrm/", title = "Tables and Graphs for Mixed Models for Repeated Measures (MMRM)" }
+pkg_info["crmPack"] = { url = "https://github.com/roche/crmPack", title = "Object-Oriented Implementation of CRM Designs" }
+pkg_info["admiral"] = { url = "https://pharmaverse.github.io/admiral/", title = "ADaM in R Asset Library" }
+pkg_info["teal.widgets"] = { url = "https://insightsengineering.github.io/teal.widgets/", title = "'shiny' Widgets for 'teal' Applications" }
+pkg_info["teal.data"] = { url = "https://insightsengineering.github.io/teal.data/", title = "Data Model for 'teal' Applications" }
+pkg_info["hermes"] = { url = "https://insightsengineering.github.io/hermes/", title = "Preprocessing, analyzing, and reporting of RNA-seq data" }
+pkg_info["goshawk"] = { url = "https://insightsengineering.github.io/goshawk/", title = "Longitudinal Visualization Functions" }
+pkg_info["admiraltemplate"] = { url = "https://pharmaverse.github.io/admiraltemplate", title = "ADaM in R Asset Library - Extension" }
+pkg_info["nestcolor"] = { url = "https://insightsengineering.github.io/nestcolor/", title = "Colors for NEST Graphs" }
+pkg_info["teal.code"] = { url = "https://insightsengineering.github.io/teal.code/", title = "Code Storage and Execution Class for 'teal' Applications" }
+pkg_info["tern.gee"] = { url = "https://insightsengineering.github.io/tern.gee/", title = "Tables and Graphs for Generalized Estimating Equations (GEE) Model Fits" }
+pkg_info["tern.rbmi"] = { url = "https://insightsengineering.github.io/tern.rbmi/", title = "Create interface for rbmi and tern" }
+pkg_info["scda.2022"] = { url = "", title = "Synthetic CDISC Data Archive 2022" }
+pkg_info["rlistings"] = { url = "https://insightsengineering.github.io/rlistings/", title = "Clinical Trial Style Data Readout Listings" }
+pkg_info["osprey"] = { url = "https://insightsengineering.github.io/osprey/", title = "R Package to Create TLGs" }
+pkg_info["teal.osprey"] = { url = "https://insightsengineering.github.io/teal.osprey/", title = "R Package of Teal Module for TLG Functions in Osprey" }
+pkg_info["teal.logger"] = { url = "https://insightsengineering.github.io/teal.logger/", title = "Logging Setup for the 'teal' Family of Packages" }
+pkg_info["scda"] = { url = "", title = "Package to Access Synthetic CDISC Archive Data" }
+pkg_info["covr"] = { url = "https://covr.r-lib.org", title = "Test Coverage for Packages" }
+pkg_info["r2rtf"] = { url = "https://merck.github.io/r2rtf/", title = "Easily Create Production-Ready Rich Text Format (RTF) Table and Figure" }
+pkg_info["rbmi"] = { url = "https://insightsengineering.github.io/rbmi/", title = "Reference Based Multiple Imputation" }
+pkg_info["visR"] = { url = "https://github.com/openpharma/visR", title = "Clinical Graphs and Tables Adhering to Graphical Principles" }
+pkg_info["riskmetric"] = { url = "https://pharmar.github.io/riskmetric/", title = "Risk Metrics to Evaluating R Packages" }
+pkg_info["sdtmchecks"] = { url = "https://pharmaverse.github.io/sdtmchecks/", title = "Data Quality Checks for SDTM Datasets" }
+pkg_info["covtracer"] = { url = "https://github.com/genentech/covtracer", title = "Tools for contextualizing tests" }
+pkg_info["simIDM"] = { url = "https://github.com/insightsengineering/simIDM/", title = "Simulating Oncology Trials using an Illness-Death Model" }
+pkg_info["admiralvaccine"] = { url = "https://pharmaverse.github.io/admiralvaccine/", title = "Vaccine Extension Package for ADaM in 'R' Asset Library" }
+pkg_info["ggplot2.utils"] = { url = "https://insightsengineering.github.io/ggplot2.utils/", title = "Selected Utilities Extending 'ggplot2'" }
+pkg_info["admiralophtha"] = { url = "https://pharmaverse.github.io/admiralophtha/", title = "ADaM in R Asset Library - Ophthalmology" }
+pkg_info["admiralonco"] = { url = "https://pharmaverse.github.io/admiralonco/", title = "Oncology Extension Package for ADaM in 'R' Asset Library" }
+pkg_info["pharmaversesdtm"] = { url = "https://pharmaverse.github.io/pharmaversesdtm/main/", title = "Test Data for the Pharmaverse Family of Packages" }
+pkg_info["ggsurvfit"] = { url = "https://www.danieldsjoberg.com/ggsurvfit/", title = "Flexible Time-to-Event Figures" }
+pkg_info["riskassessment"] = { url = "https://github.com/pharmaR/riskassessment", title = "A web app designed to interface with the `riskmetric` package" }
+pkg_info["tidytlg"] = { url = "https://github.com/pharmaverse/tidytlg", title = "Create TLGs using the 'tidyverse'" }
+pkg_info["logrx"] = { url = "https://github.com/pharmaverse/logrx", title = "A Logging Utility Focus on Clinical Trial Programming Workflows" }
+pkg_info["hal9001"] = { url = "https://github.com/tlverse/hal9001", title = "The Scalable Highly Adaptive Lasso" }
+pkg_info["MAIC"] = { url = "", title = "Package to Perform Matched-adjusted Indirect Comparisons" }
+pkg_info["admiral.test"] = { url = "", title = "Test Data for the 'admiral' Package" }
+pkg_info["staged.dependencies"] = { url = "", title = "Install R packages from Particular Git Branches" }
+pkg_info["metacore"] = { url = "https://atorus-research.github.io/metacore/", title = "A Centralized Metadata Object Focus on Clinical Trial Data Programming Workflows" }
+pkg_info["gemtcPlus"] = { url = "", title = "Provides a suite of extension functions for NMA using the `gemtc` package" }
+pkg_info["envsetup"] = { url = "", title = "Support the Setup of the R Environment for Clinical Trial Programming Workflows" }
+pkg_info["flexsurvPlus"] = { url = "", title = "Provides Functions to Perform Survival Analysis for Economic models" }
+pkg_info["tidyCDISC"] = { url = "https://Biogen-Inc.github.io/tidyCDISC/", title = "Quick Table Generation & Exploratory Analyses on ADaM-Ish Datasets" }
+pkg_info["datacutr"] = { url = "", title = "SDTM Datacut" }
+pkg_info["syntrial"] = { url = "", title = "synthesize data from real clinical trial" }
+pkg_info["metatools"] = { url = "https://pharmaverse.github.io/metatools/", title = "Enable the Use of 'metacore' to Help Create and Check Dataset" }
+pkg_info["uniCATE"] = { url = "", title = "NA" }
+pkg_info["psborrow"] = { url = "", title = "Bayesian Dynamic Borrowing with Propensity Score" }
+pkg_info["mtdesign"] = { url = "https://github.com/openpharma/mtdesign", title = "Mander and Thompson Designs" }
+pkg_info["scda.2021"] = { url = "", title = "Synthetic CDISC Data Archive 2021" }
+pkg_info["CTP"] = { url = "", title = "Closed Testing Procedure (CTP)" }
+pkg_info["origami"] = { url = "https://tlverse.org/origami/", title = "Generalized Framework for Cross-Validation" }
+pkg_info["descem"] = { url = "", title = "Discrete Event Simulation for Cost-Effectiveness Modelling" }
+pkg_info["rpsftmPlus"] = { url = "", title = "Extensions To Rpsftm Package" }
+pkg_info["nesttemplate"] = { url = "https://insightsengineering.github.io/nesttemplate/", title = "`pkgdown` template for NEST packages" }
+pkg_info["sl3"] = { url = "", title = "NA" }
+pkg_info["valtools"] = { url = "", title = "Automate Validated Package Creation" }
+pkg_info["respectables"] = { url = "", title = "Create Relationally-Specified Multi-Table Datasets" }
+pkg_info["pharmaRTF"] = { url = "", title = "Enhanced RTF Wrapper for Use with Existing Table Packages" }
+pkg_info["facetsr"] = { url = "https://github.com/openpharma/facetsr", title = "facets R wrapper" }
+pkg_info["RDO"] = { url = "https://github.com/openpharma/RDO#readme", title = "Reproducible Data Objects" }
+
+return {
+  ['pkg'] = function(args, kwargs, meta)
+    local name = args[1]
+    local url = pkg_info[name]["url"]
+    local title = pkg_info[name]["title"]
+
+    return pandoc.RawInline("html", "<a href='" .. url .. "' title = '" .. title .. "'>" .. "{" .. name .. "}" .. "</a>")
+  end
+}
