7aaf14839154a8458f1ccd8d327824e16d51eef7
Edoardo Mancini <edoardo.mancini@roche.com>
2024-08-06 16:05:42 +0000

    #196 testing changes

--- R/check_post_tags.R
+++ R/check_post_tags.R
@@ -2,15 +2,14 @@
 posts <- list.files("posts", recursive = TRUE, pattern = "*.qmd")
 
 # Function to extract tags from a post and check them against the allowed list ----
-check_post_tags <- function(post){
-
+check_post_tags <- function(post) {
   allowable_tags <- c("Metadata", "SDTM", "ADaM", "TLG", "Shiny", "Community", "Conferences", "Submissions", "Technical")
 
   post_tags <- rmarkdown::yaml_front_matter(file.path("posts", post))$categories
 
   cross_check <- post_tags %in% allowable_tags
 
-  if(!all(cross_check)){
+  if (!all(cross_check)) {
     stop(paste("The tags selected in:", post, "are not all from the allowed list of tags."))
   }
 }
