3e1d5fefda2304b5c1c150b41e84d6ea98d5d083
Edoardo Mancini <edoardo.mancini@roche.com>
2024-08-07 09:44:22 +0000

    #196 updates following review

--- R/check_post_tags.R
+++ R/check_post_tags.R
@@ -1,18 +1,34 @@
 # Get list of blog posts ----
 posts <- list.files("posts", recursive = TRUE, pattern = "*.qmd")
 
+# Get vector of allowed tags ----
+source("R/allowed_tags.R")
+
 # Function to extract tags from a post and check them against the allowed list ----
 check_post_tags <- function(post) {
-  allowable_tags <- c("Metadata", "SDTM", "ADaM", "TLG", "Shiny", "Community", "Conferences", "Submissions", "Technical")
-
   post_tags <- rmarkdown::yaml_front_matter(file.path("posts", post))$categories
 
   cross_check <- post_tags %in% allowable_tags
 
+  problematic_tags <- post_tags[!cross_check]
+
   if (!all(cross_check)) {
-    stop(paste("The tags selected in:", post, "are not all from the allowed list of tags."))
+    cli::format_message('The tag(s) {.val {problematic_tags}} in the post {.val {post}} are not from the allowable list of tags.')
   }
 }
 
-# Apply check_post_tags to all blog posts ----
-invisible(lapply(posts, check_post_tags))
+# Apply check_post_tags to all blog posts and find problem posts ----
+check_results <- lapply(posts, check_post_tags)
+error_messages <- unlist(Filter(Negate(is.null), check_results))
+
+# Construct error message if one or more posts have problematic tags ----
+if(length(error_messages) > 0){
+
+  error_messages <- c(error_messages, "Please select from the following tags: {.val {allowed_tags}}, or contact one of the maintainers.")
+  names(error_messages) = rep("x", length(error_messages) - 1)
+
+  concatenated_error_messages = cli::cli_bullets(error_messages)
+
+  cli::cli_abort(concatenated_error_messages)
+
+}
--- R/create_blogpost.R
+++ R/create_blogpost.R
@@ -1,13 +1,13 @@
 # You can easily create a new blog post skeleton using the create_post() function.
-# First, we load the helping function:
+# First, we load the helping function and the allowed post tags:
 
 source("R/help_create_blogpost.R")
-
+source("R/allowed_tags.R")
 
 ## The available images are at the moment:
 list.files("media") %>% tools::file_path_sans_ext()
 ## Please choose (possibly multiple) from this tags list:
-c("Metadata", "SDTM", "ADaM", "TLG", "Shiny", "Community", "Conferences", "Submissions", "Technical")
+print(allowed_tags)
 
 # Fill in the info, e.g.:
 create_post(
@@ -16,9 +16,8 @@ create_post(
   description = "", # you can fill the description in later as well
   author = c("My Name"), # one or more authors are permitted
   cover_image = "pharmaverse", # chose one of the above (see line 8)
-  tags = c("submission", "ADaMs", "conferences", "admiral", "xportr", "metatools", "metacore") # chose (multiple) from line 10
+  tags = c("Metadata", "SDTM", "ADaM", "TLG", "Shiny", "Community", "Conferences", "Submissions", "Technical") # chose (multiple) from line 10
 )
-
 
 # Remove that post:
 # unlink("posts/2023-06-15_lbla", recursive = TRUE)
