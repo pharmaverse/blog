d2d7dc244adaebb22dd5be4fc5a9148545fcaa8b
Edoardo Mancini <53403957+manciniedoardo@users.noreply.github.com>
2024-08-07 14:57:13 +0100

    Closes #196 Check post tags using CICD (#208)
    * #196 add function to check post tags
    
    * #196 add check post tags workflow
    
    * #196 update to workflow
    
    * #196 testing changes
    
    * #196 updates
    
    * #196 updates
    
    * #196 test
    
    * #196 test
    
    * #196 test
    
    * #196 test
    
    * #196 update
    
    * #196 update
    
    * #196 updates following review
    
    * #196 update
    
    * #196 updates
    
    * #196 updates
    
    * #196 styler
    
    * #196 styler
    
    * #196 wording
    
    * #196 styler again lol
    
    * #196 remove superassignment and linebreak in PR guidance
    
    * #196 correct submission tags

---
+++ R/allowed_tags.R
@@ -0,0 +1,4 @@
+#' This is a vector of all the allowed blog post tags. It is stored in a separate file so that it can be referenced
+#' by `create_blogpost.R` and `check_post_tags.R`.
+
+allowed_tags <- c("Metadata", "SDTM", "ADaM", "TLG", "Shiny", "Community", "Conferences", "Submissions", "Technical")
