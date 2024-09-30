4df15152b56dd363ee9406432eda957aaa9070d5
Vladyslav <shuliarv@roche.com>
2024-09-30 14:09:18 +0000

    #234 Reverted create_blogpost changes

--- R/create_blogpost.R
+++ R/create_blogpost.R
@@ -11,12 +11,12 @@ print(allowed_tags)
 
 # Fill in the info, e.g.:
 create_post(
-  post_name = "How I Rebuilt a Lost ECG Data Script in R", # needs to be character vector (length 1)
-  post_date = "2024-09-30", # needs to be length 1 character vector and will be auto-updated when your post is merged
-  description = "A Data Science placement student shares their experience of rewriting a lost R script to regenerate an essential ECG dataset for the open-source *pharmaversesdtm* project. The post covers their approach to data exploration, identifying key parameters, and overcoming challenges in recreating the dataset from scratch.", # you can fill the description in later as well
-  author = c("Vladyslav Shuliar"), # one or more authors are permitted
+  post_name = "My Blog", # needs to be character vector (length 1)
+  post_date = "2024-12-01", # needs to be length 1 character vector and will be auto-updated when your post is merged
+  description = "", # you can fill the description in later as well
+  author = c("My Name"), # one or more authors are permitted
   cover_image = "pharmaverse", # chose one of the above (see line 8)
-  tags = c("SDTM", "Community", "Technical") # chose (multiple) from line 10
+  tags = c("Metadata", "SDTM", "ADaM", "TLG", "Shiny", "Community", "Conferences", "Submissions", "Technical") # chose (multiple) from line 10
 )
 
 # Remove that post:
