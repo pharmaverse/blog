c112049aab69387182d0f47489023b6cd7a9ada1
alekoure <akourets@gmail.com>
2024-10-07 23:19:21 +0000

    Adding floating point paragraph

--- posts/zzz_DO_NOT_EDIT_the__tensio.../mirai_workflow.R
+++ posts/zzz_DO_NOT_EDIT_the__tensio.../mirai_workflow.R
@@ -1,17 +1,17 @@
 {
     library("mirai")
-    library("dplyr", warn.conflicts = FALSE)
+    library("dplyr")
     log_file <- tempfile()
     mirai::daemons(4)
     mirai::everywhere({
-        library("dplyr", warn.conflicts = FALSE)
-        library("tidylog", warn.conflicts = FALSE)
+        library("dplyr")
+        library("tidylog")
         log_to_file <- function(txt) cat(txt, file = log_file, 
             sep = "\n", append = TRUE)
         options(tidylog.display = list(message, log_to_file))
     }, log_file = log_file)
     m <- mirai_map(letters[1:5], function(x) {
-        mutate(tidyr::tibble(.rows = 1), `:=`("{x}", "foo"))
+        mutate(tibble(.rows = 1), `:=`("{x}", "foo"))
     })
     result <- dplyr::bind_cols(m[])
     mirai::daemons(0)
