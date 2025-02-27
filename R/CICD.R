019bc952acb56e0f53bbbf167128c0f8453f227d
Stefan Pascal Thoma <stefan.thoma@roche.com>
2025-02-27 15:19:30 +0000

    update spellcheck and CICD prep file

--- R/CICD.R
+++ R/CICD.R
@@ -1,34 +1,57 @@
-#-------------------------- spell-check ----------------------------------------
+# Load required packages
+library(spelling)
+library(readr)
+library(stringr)
+library(dplyr)
 
-# create empty wordlist:
-# write("", file =   "inst/WORDLIST.txt")
-# check spelling:
-spelling::spell_check_files(list.files(pattern = ".*.qmd$", recursive = TRUE),
-  ignore = readr::read_lines("inst/WORDLIST.txt")
-)
+#-------------------------- Spell-check Without Modifying Files --------------------------
 
-# now check those words and whether or not they are really mistakes.
-# once you fixed all mistakes you can:
-words <- spelling::spell_check_files(list.files(pattern = ".*.qmd$", recursive = TRUE),
-  ignore = readr::read_lines("inst/WORDLIST.txt")
-)
-# now you can add words to the wordlist
-#-- uncomment the following line
-# write(words[[1]], file =   "inst/WORDLIST.txt", append = TRUE)
-# sort_words <- sort(readr::read_lines("inst/WORDLIST.txt"))
-# readr::write_lines(sort_words, "inst/WORDLIST.txt")
+# Function to read and clean text (remove URLs in-memory)
+clean_text_for_spellcheck <- function(file) {
+  text <- read_lines(file)
+  text_clean <- str_replace_all(text, "https?://[^\\s)\"']+", "")  # Remove URLs
+  return(text_clean)
+}
 
-spelling::spell_check_files(list.files(pattern = ".*.qmd$", recursive = TRUE),
-  ignore = readr::read_lines("inst/WORDLIST.txt")
-)
+# Get all .qmd files
+qmd_files <- list.files(pattern = ".*\\.qmd$", recursive = TRUE)
 
-#-------------------------- style-check ----------------------------------------
+# Run spell check per file and store results
+spell_check_results <- lapply(qmd_files, function(file) {
+  cleaned_text <- clean_text_for_spellcheck(file)
+  words <- spelling::spell_check_text(cleaned_text, ignore = read_lines("inst/WORDLIST.txt"))
 
-# this is what happens in CICD:
+  if (nrow(words) > 0) {
+    words$file <- file  # Add filename column
+  }
+
+  return(words)
+})
+
+# Combine results into a single dataframe
+all_typos <- bind_rows(spell_check_results)
+
+# Print results
+if (nrow(all_typos) > 0) {
+  print(all_typos %>% select(file, word, line))
+} else {
+  message("No spelling errors found!")
+}
+
+#-------------------------- Add Words to Wordlist (If Needed) --------------------------
+
+# Uncomment if you want to manually add words to the wordlist
+# write(all_typos$word, file = "inst/WORDLIST.txt", append = TRUE)
+# sort_words <- sort(read_lines("inst/WORDLIST.txt"))
+# write_lines(sort_words, "inst/WORDLIST.txt")
+
+#-------------------------- Style-check ----------------------------------------
+
+# This is what happens in CI/CD:
 styler::style_dir(dry = "fail")
 
-# fix it like this:
+# Fix it locally:
 styler::style_dir()
 
-# now it should work:
+# Now it should pass:
 styler::style_dir(dry = "fail")
--- R/create_blogpost.R
+++ R/create_blogpost.R
@@ -11,12 +11,13 @@ print(allowed_tags)
 
 # Fill in the info, e.g.:
 create_post(
-  post_name = "My Blog", # needs to be character vector (length 1)
-  post_date = "2024-12-01", # needs to be length 1 character vector and will be auto-updated when your post is merged
+  post_name = "Automatic slide generation with {autoslideR}", # needs to be character vector (length 1)
+  post_date = "2025-03-01", # needs to be length 1 character vector and will be auto-updated when your post is merged
   description = "", # you can fill the description in later as well
-  author = c("My Name"), # one or more authors are permitted
+  author = c("Stefan P. Thoma"), # one or more authors are permitted
   cover_image = "pharmaverse", # chose one of the above (see line 8)
-  tags = c("Metadata", "SDTM", "ADaM", "TLG", "Shiny", "Community", "Conferences", "Submissions", "Technical") # chose (multiple) from line 10
+  tags = "TLG"
+  # tags = c("Metadata", "SDTM", "ADaM", "TLG", "Shiny", "Community", "Conferences", "Submissions", "Technical") # chose (multiple) from line 10
 )
 
 # Remove that post:
