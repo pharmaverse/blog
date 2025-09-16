1cd74d57b7871e729def0417ccf7a93c7d4f11ae
Edoardo Mancini <edoardo.mancini@roche.com>
2025-09-16 07:51:42 +0000

    Manual publish

--- posts/2024-10-16_the__tensio.../mirai_workflow.R
+++ posts/2024-10-16_the__tensio.../mirai_workflow.R
@@ -1,26 +1,19 @@
 {
-  library("mirai")
-  library("dplyr")
-  log_file <- tempfile()
-  mirai::daemons(4)
-  mirai::everywhere(
-    {
-      library("dplyr")
-      library("tidylog")
-      log_to_file <- function(txt) {
-        cat(txt,
-          file = log_file,
-          sep = "\n", append = TRUE
-        )
-      }
-      options(tidylog.display = list(message, log_to_file))
-    },
-    log_file = log_file
-  )
-  m <- mirai_map(letters[1:5], function(x) {
-    mutate(tibble(.rows = 1), `:=`("{x}", sample(1:100, 1)))
-  })
-  result <- bind_cols(m[])
-  mirai::daemons(0)
-  print(list(logs = readLines(log_file), result = result))
+    library("mirai")
+    library("dplyr")
+    log_file <- tempfile()
+    mirai::daemons(4)
+    mirai::everywhere({
+        library("dplyr")
+        library("tidylog")
+        log_to_file <- function(txt) cat(txt, file = log_file, 
+            sep = "\n", append = TRUE)
+        options(tidylog.display = list(message, log_to_file))
+    }, log_file = log_file)
+    m <- mirai_map(letters[1:5], function(x) {
+        mutate(tibble(.rows = 1), `:=`("{x}", sample(1:100, 1)))
+    })
+    result <- bind_cols(m[])
+    mirai::daemons(0)
+    print(list(logs = readLines(log_file), result = result))
 }
